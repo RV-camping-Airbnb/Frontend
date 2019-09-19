d19a1aa26c661c44373c8c213e5851fe590095b2
ndurbin <nick.durbin@gmail.com>
2019-09-19 15:11:46 -0400

    Styled the forms with material UI and implemented formik within them.

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,24 +1,135 @@
-import React, { useState } from 'react';
+import React, {useState } from 'react';
+import { Link } from 'react-router-dom';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        RVNB
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  Form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function LoginForm({ values, errors, touched, isSubmitting }) {
   const [users, setUsers] = useState([])
+  const classes = useStyles();
 
   return (
-    <>
-    <Form className="loginForm">
-      <h1>Login Form</h1>
-
-      {touched.email && errors.email && <p className="error">{errors.email}</p>}
-      <Field type="email" name="email" placeholder="Email" />
-
-      {touched.password && errors.password && <p className="error">{errors.password}</p>}
-      <Field type="password" name="password" placeholder="Password" />
-
-      <button className="formButton" type="submit" disabled={isSubmitting}>Submit</button>
-    </Form>
-     </>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Form className={classes.form} noValidate>
+          {touched.email && errors.email && <p className="error">{errors.email}</p>}
+            <TextField
+              type="email"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            {touched.password && errors.password && <p className="error">{errors.password}</p>}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button 
+              disabled={isSubmitting}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to='/signup' variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </Form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/Login/SignUpForm.js
+++ src/components/Login/SignUpForm.js
@@ -1,63 +1,175 @@
-import React, { useState } from 'react';
+import React, {useState } from 'react';
+import { Link } from 'react-router-dom';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        RVNB
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  Form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function SignUpForm({ values, errors, touched, isSubmitting }) {
-
   const [users, setUsers] = useState([])
   console.log(users)
+  const classes = useStyles();
 
   return (
-    <>
-    <Form className="signupForm">
-      <h1>Sign-Up Form</h1>
-
-      {touched.fname && errors.fname && <p className="error"> {errors.fname}</p>}
-      <Field type="fname" name="fname" placeholder="First Name" />
-
-      {touched.lname && errors.lname && <p className="error"> {errors.lname}</p>}
-      <Field type="lname" name="lname" placeholder="Last Name" />
-
-      {touched.email && errors.email && <p className="error">{errors.email}</p>}
-      <Field type="email" name="email" placeholder="Email" />
-
-      {touched.password && errors.password && <p className="error">{errors.password}</p>}
-      <Field type="password" name="password" placeholder="Password" />
-
-      <label>
-        {touched.landowner && errors.landowner && <p className="error">{errors.landowner}</p>}
-        <Field className="checkbox" type="checkbox" name="landowner" checked={values.landowner} />
-        <span>Land Owner</span>
-      </label>
-
-      <label>
-        {touched.rvowner && errors.rvowner && <p className="error">{errors.rvowner}</p>}
-        <Field className="checkbox" type="checkbox" name="rvowner" checked={values.rvowner} />
-        <span>RV Owner</span>
-      </label>
-
-      <label>
-        {touched.tos && errors.tos && <p className="error">{errors.tos}</p>}
-        <Field className="checkbox" type="checkbox" name="tos" checked={values.tos} />
-        <span>Accept Terms</span>
-      </label>
-
-      <button className="formButton" type="submit" disabled={isSubmitting}>Submit</button>
-    </Form>
-    {users.map((user, index) => {
-        return (
-          <div className='userContainer' key={index} index={index}>
-            <h1>New User Info</h1>
-            <h2>Name: {user.fname} {user.lname}</h2>
-            <h4>Email: {user.email}</h4>
-          </div>
-      ) })}
-     </>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Form className={classes.form} noValidate>
+            {touched.fname && errors.fname && <p className="error"> {errors.fname}</p>}
+            <TextField 
+              type="fname" 
+              name="fname" 
+              label="First Name"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="fname"
+              autoFocus
+            />
+            {touched.lname && errors.lname && <p className="error"> {errors.lname}</p>}
+            <TextField 
+              type="lname" 
+              name="lname" 
+              label="Last Name"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="lname"
+              autoFocus 
+            />
+            {touched.email && errors.email && <p className="error">{errors.email}</p>}
+            <TextField
+              type="email"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            {touched.password && errors.password && <p className="error">{errors.password}</p>}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="RV Owner" color="primary" />}
+              label="RV Owner"
+              type="checkbox" 
+              name="rvowner" 
+              checked={values.rvowner}
+            />
+             <FormControlLabel
+              control={<Checkbox value="Land Owner" color="primary" />}
+              label="Land Owner"
+              type="checkbox" 
+              name="landowner" 
+              checked={values.landowner}
+            />
+            <Button 
+              disabled={isSubmitting}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to='/signup' variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </Form>
+        </div>
+      </Grid>
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+    </Grid>
   );
 }
 
 export default withFormik({
-  mapPropsToValues({ fname, lname, email, password, landowner, rvowner, tos }) {
+  mapPropsToValues({ fname, lname, email, password, landowner, rvowner }) {
     
     return {
       fname: fname || "",
@@ -66,7 +178,6 @@ export default withFormik({
       password: password || "",
       landowner: landowner || false,
       rvowner: rvowner || false,
-      tos: tos || false
     };
   },
 
@@ -85,7 +196,6 @@ export default withFormik({
       .required("Password is required"),
     landowner: Yup.boolean().oneOf([ true ], "Please check if you are a land owner"),
     rvowner: Yup.boolean().oneOf([ true ], "Please check if you are an RV ownver"),
-    tos: Yup.boolean().oneOf([ true ], "Please agree to Terms of Service!")
   }),
 
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
