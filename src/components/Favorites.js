298ea4214b0008fb17b939583572fba2fd9c2d8f
ndurbin <nick.durbin@gmail.com>
2019-09-20 20:32:13 -0400

    The code to handle the save click is in place.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Favorites = props => (
-  <div className="saved-list">
-    <h3>Favorite Listings:</h3>
-    {props.favoriteList.map(listing => (
-      <span className="saved-listing">{listing.title}</span>
-    ))}
-  </div>
-);
+const Favorites = (props) => {
+  const [listing, setListing] = useState();
+ 
+  const saveListing = () => {
+    const addToFavoritesList = props.addToFavoritesList;
+    addToFavoritesList(listing)
+  }
+
+  return (
+    <div className="saved-list">
+      <h3>Favorite Listings:</h3>
+      {props.favoriteList.map(listing => (
+        <div>
+          <span className="saved-listing">{listing.title}</span>
+          <button className="save-button" onClick={() => saveListing()}>Save</button>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Favorites;
