217d76dcf3ce25df34930f58b6b29eb47f784ad6
ndurbin <nick.durbin@gmail.com>
2019-09-22 03:06:45 -0400

    Added a skeleton to display a favorites list with basic styling and data.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
+    justifyContent: 'flex-start',
     margin: '0 auto',
     width: '80%',
     height: '500px',
@@ -27,14 +29,87 @@ const useStyles = makeStyles(theme => ({
     width: 200,
     height: 200,
     backgroundColor: 'gray',
-    border: '2px solid black'
+    border: '2px solid black',
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  example: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '1% auto',
+    width: '80%',
+    padding: '1%'
+  },
+  grid: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '80%',
+    padding: '1%'
+  },
+  favContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '1% auto',
+    width: '100%',
+    padding: '1%'
+  },
+  heading: {
+    fontSize:'1.6rem',
+    margin: '0 auto'
   },
 }));
 
-function Profile() {
+const data = [
+  {
+    src: 'https://source.unsplash.com/random',
+    title: 'Property One',
+    location: 'San Jose, CA',
+    views: '396 k views',
+    createdAt: 'a week ago',
+  },
+  {
+    src: 'https://source.unsplash.com/random',
+    title: 'Property Two',
+    location: 'Paris, France',
+    views: '2.1 M views',
+    createdAt: '4 months ago',
+  },
+  {
+    src: 'https://source.unsplash.com/random',
+    title: 'Property Three',
+    location: 'Tampa, FL',
+    views: '130 M views',
+    createdAt: '10 months ago',
+  },
+  {
+    src: 'https://source.unsplash.com/random',
+    title: 'Property One',
+    location: 'San Jose, CA',
+    views: '396 k views',
+    createdAt: 'a week ago',
+  },
+  {
+    src: 'https://source.unsplash.com/random',
+    title: 'Property Two',
+    location: 'Paris, France',
+    views: '2.1 M views',
+    createdAt: '4 months ago',
+  },
+];
+
+function Profile(props) {
   const classes = useStyles();
+  const { loading = false } = props;
 
   return (
+    <>
     <Paper className={classes.root}>
       <Paper className={classes.bio}>
         <Avatar alt="Nick Durbin" src="" className={classes.bigAvatar} />
@@ -43,6 +118,41 @@ function Profile() {
         <Rating name="half-rating" value={4.5} precision={0.5} />
       </Paper>
     </Paper>
+    <Paper className={classes.example}>
+    <h1 className={classes.heading}>Favorite Properties</h1>
+    <Grid container wrap="nowrap" className={classes.grid}>
+    {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+      <Box key={index} width={260} margin={1} my={5}>
+        {item ? (
+          <img style={{ width: 220, height: 140 }} alt={item.title} src={item.src} />
+        ) : (
+          <Skeleton variant="rect" width={260} height={140} />
+        )}
+
+        {item ? (
+          <Box paddingRight={2} className={classes.favContent}>
+            <Typography gutterBottom variant="body2">
+              {item.title}
+            </Typography>
+            <Typography display="block" variant="caption" color="textSecondary">
+              {item.location}
+            </Typography>
+            <Typography variant="caption" color="textSecondary">
+              {`${item.views} • ${item.createdAt}`}
+            </Typography>
+            <Rating name="half-rating" value={4.5} precision={0.5} />
+          </Box>
+        ) : (
+          <React.Fragment>
+            <Skeleton />
+            <Skeleton width="60%" />
+          </React.Fragment>
+        )}
+      </Box>
+      ))}
+    </Grid>
+  </Paper>
+  </>
   )
 }
 
